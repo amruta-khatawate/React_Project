1a1659796503cf6c401fd01e2afbd915f023b0dc
amruta khatawate <amrutakhatawate206@gmail.com>
2025-03-13 21:51:37 +0530

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <div>
+    <p>Hello world</p>
+  </div>
+  <Text/>
+    <div>
+    <p>Hello world</p>
+  </div>
+  <div>
+    <p>Hello world</p>
+  </div>
+  <Text/>
+  </>
+  )
+}
+
+function Text(){
+  return (
+    <div>
+      <p>Hello world2</p>
+    </div>
+  )
+}
+
+export default App
